006ea40e6d639e68a5d4d0b372d1286b6f211b34
Sony Mathew <32841648+SonyMC@users.noreply.github.com>
2023-08-10 18:43:11 +0530

    Aug10_2023

--- src/main/java/com/mailsonymathew/microservices/currencyexchangeservicev2/controllers/CircuitBreakerController.java
+++ src/main/java/com/mailsonymathew/microservices/currencyexchangeservicev2/controllers/CircuitBreakerController.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+
 
 @RestController
 public class CircuitBreakerController {
@@ -21,14 +19,14 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@GetMapping("/sample-api")
-	//*** Retry - Allows us to configure teh retry attemps
-	// @Retry(name = "default") // use default Retry configuration  -> will retry 3 times
+	//*** Retry - Allows us to configure the retry attempts
+	//@Retry(name = "default") // use default Retry configuration  -> will retry 3 times
 	// @Retry(name="sample-api") // use this name in application.properties to configure retry count 
-	//@Retry(name="sample-api", fallbackMethod = "hardCodedResponse")  // use this name in application.properties to configure retry count ;define publi method 'hardCodedResponse'
+	//@Retry(name="sample-api", fallbackMethod = "hardCodedResponse")  // use this name in application.properties to configure retry count ;define public method 'hardCodedResponse'
 	//@CircuitBreaker(name="sample-api", fallbackMethod = "hardCodedResponse") // Circuit Breaker is used when the microservice is entirely down. After repeated attempts, circuit breaker will not call the dependant api & send a default response
 	//*** CircuitBreaker - Allows us to configure the maximum number of calls allowed for a given interval
 	//@RateLimiter(name="sample-api")
-	//*** Bulkhead - Allows us to configure maximum concurrent calls allowd
+	//*** Bulkhead - Allows us to configure maximum concurrent calls allowed
 	// @Bulkhead(name="sample-api")
 	public String sampleAPI() {
 		
@@ -41,7 +39,7 @@ public class CircuitBreakerController {
 		return forEntity.getBody();
 	}
 
-	// This methodis a fallback method as defined in the @Retry annotation of the sampleAPI() method above
+	// This method is a fallback method as defined in the @Retry annotation of the sampleAPI() method above
 	public String hardCodedResponse(Exception ex) {
 	//public String hardCodedResponse(RuntimeException ex) {
 		return "Oops!!! Something went wrong. Please try again later...";
